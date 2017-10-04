f4d7d0168f48fc00dcaab76e7757dc84d62468a8
queicherius <queicherius@gmail.com>
2017-10-04 08:48:49 +0100

    Small cleanup and refactoring

--- src/crawlPatreons.js
+++ src/crawlPatreons.js
@@ -4,23 +4,20 @@ const execall = require('execall')
 const cheerio = require('cheerio')
 
 async function crawlPatreons (options) {
-  const cookie = await getSessionCookie(options)
-  debug(`logged in with session cookie`, cookie)
-
-  const billingSchedules = await getBillingSchedules(cookie)
-  debug(`got ${Object.values(billingSchedules).length} billing schedules`)
+  const authFetch = await getAuthenticatedRequestHandler(options)
+  const billingSchedules = await getBillingSchedules(authFetch)
 
   let patreons = {}
   for (let i = 0; i !== billingSchedules.length; i++) {
     const {text, href} = billingSchedules[i]
-    debug(`parsing table of '${text}' (${i + 1} / ${billingSchedules.length})`)
-    patreons[text] = await parsePatreonManager(cookie, href)
+    debug(`parsing schedule of '${text}' (${i + 1} / ${billingSchedules.length})`)
+    patreons[text] = await parsePatreonManager(authFetch, href)
   }
 
   return patreons
 }
 
-async function getSessionCookie (options) {
+async function getAuthenticatedRequestHandler (options) {
   const response = await fetch('https://www.patreon.com/api/login', {
     method: 'POST',
     body: JSON.stringify({
@@ -31,6 +28,7 @@ async function getSessionCookie (options) {
     })
   })
 
+  // Parse the cookie header into something usable
   const cookies = response.headers._headers['set-cookie']
     .map(cookie => cookie.replace(/^(.*?); .+$/, '$1'))
     .map(cookie => {
@@ -38,53 +36,53 @@ async function getSessionCookie (options) {
       return {name: cookie[0], value: cookie[1]}
     })
 
+  // Find the ssid and session_id cookies required for making authenticated requests
   const ssid = cookies.find(cookie => cookie.name === 'patreon_device_id').value
   const session_id = cookies.filter(cookie => cookie.name === 'session_id')[1].value
-  return `__ssid=${ssid}; session_id=${session_id}`
+  const cookie = `__ssid=${ssid}; session_id=${session_id}`
+  debug(`logged in with session cookie`, cookie)
+
+  // Build a wrapper function to send requests with
+  async function authenticatedRequest (url) {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {cookie: cookie}
+    })
+
+    return await response.text()
+  }
+
+  return authenticatedRequest
 }
 
-async function getBillingSchedules (cookie) {
-  const response = await fetch('https://www.patreon.com/manageRewards', {
-    method: 'GET',
-    headers: {cookie: cookie}
-  })
-  const content = await response.text()
+async function getBillingSchedules (authFetch) {
+  const content = await authFetch('https://www.patreon.com/manageRewards')
 
   // Parse out all links to the reward lists
   const regex = /<a href="(\/manageRewardsList\?billing_cycle=[^"]+)">([\w ]*)<\/a>/g
   const matches = execall(regex, content)
+  debug(`parsed ${matches.length} billing schedules`)
 
   // Convert into a little nicer matches with full URLs
   return matches.map(x => ({href: `https://www.patreon.com${x.sub[0]}`, text: x.sub[1]}))
 }
 
-async function parsePatreonManager (cookie, href) {
-  const response = await fetch(href, {
-    method: 'GET',
-    headers: {cookie: cookie}
-  })
-  const content = await response.text()
+async function parsePatreonManager (authFetch, href) {
+  const content = await authFetch(href)
 
   // Parse out all ajax requests
   const regex = /data-manage-rewards-list-page-url="([^"]*)"/g
-  const matches = execall(regex, content)
-
-  const requests = matches
+  const ajaxRequests = execall(regex, content)
     .map(x => x.sub[0].replace(/&amp;/g, '&'))
     .map(x => `https://www.patreon.com${x}`)
 
   // Go through the ajax requests and parse the patreon tables in them
-  const patreons = await Promise.all(requests.map(href => parsePatreonTable(cookie, href)))
+  const patreons = await Promise.all(ajaxRequests.map(url => parsePatreonTable(authFetch, url)))
   return patreons.reduce((arr, elem) => arr.concat(elem), [])
 }
 
-async function parsePatreonTable (cookie, href) {
-  const response = await fetch(href, {
-    method: 'GET',
-    headers: {cookie: cookie}
-  })
-  const content = await response.text()
-
+async function parsePatreonTable (authFetch, href) {
+  const content = await authFetch(href)
   const $ = cheerio.load(`<table>${content}</table>`)
 
   return $('tr').toArray().map(elem => {
